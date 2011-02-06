b7578321ed8856d3e904874aca08e2769cd3b929
瀧内 元気 <genki@s21g.com>
2011-02-06 15:55:03 +0900

    Makes project be a proguard project by using proguard plugin.

--- project/build/RubyistProject.scala
+++ project/build/RubyistProject.scala
@@ -1,7 +1,15 @@
 import sbt._
 
-class RubyistProject(info: ProjectInfo) extends DefaultProject(info) {
+class RubyistProject(info: ProjectInfo) extends ProguardProject(info) {
   val scalaTest = "org.scalatest" % "scalatest" % "1.2"
   val commonsIO = "commons-io" % "commons-io" % "2.0"
+
+  def keepMainClass = """
+    -keepclasseswithmembers public class * {
+      public static void main(java.lang.String[]);
+    }
+    """
+  override def proguardDefaultArgs = "-dontwarn" :: "-dontoptimize" ::
+    "-dontobfuscate" :: keepMainClass  ::  proguardOptions
 }
 
